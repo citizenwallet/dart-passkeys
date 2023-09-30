66dfd04d6f586e51c4cbd1cdfeda67c7f43d5e39
kev-techi <kevin@techi.be>
2023-09-30 14:17:17 +0200

    [added] dist

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: "main.js",
+    filename: "passkeys.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
