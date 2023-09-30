2f94def69c37a38ee097a9c77809f7f43355b12c
kev-techi <kevin@techi.be>
2023-09-30 22:49:22 +0200

    [wip] create method

--- web/index.js
+++ web/index.js
@@ -9,6 +9,7 @@ window.jsInvokeMethod = async (method, params) => {
       console.log("Hello World from js!");
       break;
     case "create":
+      console.log("hello");
       return create(params);
     default:
       console.log("Method not found", method);
@@ -23,11 +24,15 @@ function callDart(event) {
 
 // Code here
 async function create(userId) {
+  console.log("create", userId);
   const publicKeyConfig = {
+    challenge: Uint8Array.from("a7c61ef9-dc23-4806-b486-2428938a547e", (c) =>
+      c.charCodeAt(0)
+    ),
     rp: { id: "xyz.citizenwallet", name: "Citizen Wallet" },
     user: {
       //   id: new Uint8Array([79, 252, 83, 72, 214, 7, 89, 26]),
-      id: userId,
+      id: Uint8Array.from(userId, (c) => c.charCodeAt(0)),
     },
     pubKeyCredParams: [{ type: "public-key", alg: -7 }],
   };
